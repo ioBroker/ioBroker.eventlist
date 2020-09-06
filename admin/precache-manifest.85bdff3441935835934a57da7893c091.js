657d4f362d8fd6554cb07a53ba8226a464d85dda
GermanBluefox <dogafox@gmail.com>
2020-09-06 21:26:33 +0200

    Work on PDF

---
+++ admin/precache-manifest.85bdff3441935835934a57da7893c091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a22efed516186ebaf65bd76266eaaa9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2818d8c6ffac8b2d2030",
+    "url": "./static/css/2.9ba4c952.chunk.css"
+  },
+  {
+    "revision": "91fd52a45332ce62eea0",
+    "url": "./static/css/main.d4bed9c6.chunk.css"
+  },
+  {
+    "revision": "2818d8c6ffac8b2d2030",
+    "url": "./static/js/2.46102119.chunk.js"
+  },
+  {
+    "revision": "b76d50d4ed8788fca3cdfd3cf3da88b8",
+    "url": "./static/js/2.46102119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fd52a45332ce62eea0",
+    "url": "./static/js/main.8d8cd920.chunk.js"
+  },
+  {
+    "revision": "b864c8bf11e7075ea4b4",
+    "url": "./static/js/runtime-main.2db515fd.js"
+  }
+]);
