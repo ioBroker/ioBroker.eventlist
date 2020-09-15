3768d8e8a91ff56f25b83075811bb9710a0f2ff0
GermanBluefox <dogafox@gmail.com>
2020-09-15 19:39:04 +0200

    Fix errors

---
+++ www/precache-manifest.e9976f4246ecbd004e3812e1651e43bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2db5600fb4b1700ee6a29e0cba1f900",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66eb9a6e5446b686a631",
+    "url": "./static/css/2.9ba4c952.chunk.css"
+  },
+  {
+    "revision": "9462ac278b53de77ebd2",
+    "url": "./static/css/main.d4bed9c6.chunk.css"
+  },
+  {
+    "revision": "66eb9a6e5446b686a631",
+    "url": "./static/js/2.d2e40a78.chunk.js"
+  },
+  {
+    "revision": "b76d50d4ed8788fca3cdfd3cf3da88b8",
+    "url": "./static/js/2.d2e40a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9462ac278b53de77ebd2",
+    "url": "./static/js/main.6ba21d0f.chunk.js"
+  },
+  {
+    "revision": "b864c8bf11e7075ea4b4",
+    "url": "./static/js/runtime-main.2db515fd.js"
+  },
+  {
+    "revision": "e000ae58e7def2ca18d983b67490c65e",
+    "url": "./static/media/pushover.e000ae58.svg"
+  },
+  {
+    "revision": "629c958a4afd6a842a081372b2cae3c2",
+    "url": "./static/media/telegram.629c958a.svg"
+  }
+]);
